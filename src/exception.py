73f52745273c4f408ccde7fa8a48d0142396ef92
24aagam <aagamshah24603@gmail.com>
2024-09-26 22:09:58 +0530

    Sensor Project

--- src/exception.py
+++ src/exception.py
@@ -16,10 +16,10 @@ def error_message_details(error, error_detail:sys):
 
 
 class CustomException(Exception):
-    def __init__(self, error_message, error_details: sys):
+    def __init__(self, error_message, error_detail: sys):
         super().__init__(error_message)
         self.error_message = error_message_details(
-            error_message,error_details=error_details
+            error_message,error_detail=error_detail
         )
 
     def __str__(self):
