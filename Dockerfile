8657f33c84101c38a040255f654df64c29ed8d6e
24aagam <aagamshah24603@gmail.com>
2025-01-01 17:41:24 +0530

    files updated

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 # Install Docker in your system and extension
 
-FROM python:3.8-slim-buster
+FROM python:3.9-slim-buster
 
 WORKDIR /app
 
--- src/constant/__init__.py
+++ src/constant/__init__.py
@@ -1,11 +1,19 @@
 import os
+from dotenv import load_dotenv
 
-AWS_S3_BUCKET_NAME = "wafer-fault"
+# Load environment variables from the .env file
+load_dotenv()
+
+# Retrieve credentials
+username = os.getenv("MONGO_USERNAME")
+password = os.getenv("MONGO_PASSWORD")
+
+AWS_S3_BUCKET_NAME = "sensor-wafer-fault"
 MONGO_DATABASE_NAME = "Aagam"
 MONGO_COLLECTION_NAME = "waferfault"
 
 TARGET_COLUMN = "quality"
-MONGO_DB_URL = "mongodb+srv://bayosol372:RodBrx8EjbcXTUaR@cluster0.mnuki.mongodb.net/?retryWrites=true&w=majority"
+MONGO_DB_URL = f"mongodb+srv://{username}:{password}@cluster0.mnuki.mongodb.net/?retryWrites=true&w=majority"
 
 
 MODEL_FILE_NAME = "model"
